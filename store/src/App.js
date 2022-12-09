f809c814f93e995758c25c7a4065719ded82d470
Ryan Kirk <ryanspacekirk@gmail.com>
2022-12-09 12:28:48 -0700

    deployment IV

--- store/src/App.js
+++ store/src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import config from './config'
 import React, {useState} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
 
 
 
@@ -9,6 +12,8 @@ import Home from './components/Home';
 import InventoryManager from './components/InventoryManager';
 import Guest from './components/Guest'
 import Header from './components/Header';
+
+const ApiUrl = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl;
 
 function App() {
   let [loggedInUser, setLoggedInUser] = useState({});
